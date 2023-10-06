7b9c3d36003fd038921cd27199882a21e1e61e01
hmattosaffinitiv <hmattos@affinitiv.com>
2023-10-06 18:16:59 -0300

    engines and pageSegModes

--- Controllers/TesseracController.cs
+++ Controllers/TesseracController.cs
@@ -10,38 +10,42 @@ public class TesseracController : ControllerBase
     [HttpPost]
     public List<OcrResult> Post(IFormFile file)
     {
-        var engineModes = new List<EngineMode>(){
-            EngineMode.Default,
-            EngineMode.LstmOnly,
-            EngineMode.TesseractAndLstm,
-            EngineMode.TesseractOnly
-        };
+        var engineModes = Enum.GetNames(typeof(EngineMode)).ToList();
+        var pageSegModes = Enum.GetNames(typeof(PageSegMode)).ToList();
 
         var result = new List<OcrResult>();
 
         foreach (var item in engineModes)
         {
-            using var eng = new TesseractEngine(@"./tessdata", "eng", item);
-
-            using var memoryStream = new MemoryStream();
-            file.CopyTo(memoryStream);
-            var fileBytes = memoryStream.ToArray();
-
-            using var img = Pix.LoadFromMemory(fileBytes);
-            using var page = eng.Process(img);
-
-            var text = page.GetText();
-            var meanConfidence = page.GetMeanConfidence();
-
-            result.Add(new OcrResult
+            foreach (var item2 in pageSegModes)
             {
-                Text = text,
-                EngineMode = item.ToString(),
-                MeanConfidence = meanConfidence
-            });
+                try
+                {
+                    using var eng = new TesseractEngine(@"./tessdata", "eng", (EngineMode)Enum.Parse(typeof(EngineMode), item, true));
+                    eng.DefaultPageSegMode = (PageSegMode)Enum.Parse(typeof(PageSegMode), item2, true);
+
+                    using var memoryStream = new MemoryStream();
+                    file.CopyTo(memoryStream);
+                    var fileBytes = memoryStream.ToArray();
+
+                    using var img = Pix.LoadFromMemory(fileBytes);
+                    using var page = eng.Process(img);
+
+                    var text = page.GetText();
+                    var meanConfidence = page.GetMeanConfidence();
+
+                    result.Add(new OcrResult
+                    {
+                        Text = text,
+                        EngineMode = item.ToString(),
+                        PageSegMode = item2.ToString(),
+                        MeanConfidence = meanConfidence
+                    });
+                }
+                catch { }
+            }
         }
 
-        return result;
-
+        return result.OrderByDescending(x => x.MeanConfidence).ToList();
     }
 }
--- OcrResult.cs
+++ OcrResult.cs
@@ -3,4 +3,5 @@ public class OcrResult
     public string? Text { get; set; }
     public float MeanConfidence { get; set; }
     public string? EngineMode { get; set; }
+    public string? PageSegMode { get; set; }
 }

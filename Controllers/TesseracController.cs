6564f2c75a80cb3d68cfabcf2c26e5a1dbf84f6c
hmattosaffinitiv <hmattos@affinitiv.com>
2023-10-06 19:29:28 -0300

    engines and pageSegModes

--- Controllers/TesseracController.cs
+++ Controllers/TesseracController.cs
@@ -7,6 +7,55 @@ namespace tesseract_test.Controllers;
 [Route("[controller]")]
 public class TesseracController : ControllerBase
 {
+
+    [HttpPost]
+    public List<OcrResult> Post2(IFormFile file)
+    {
+        var engineModes = Enum.GetNames(typeof(EngineMode)).ToList();
+        var pageSegModes = Enum.GetNames(typeof(PageSegMode)).ToList();
+
+        var result = new List<OcrResult>();
+
+        foreach (var engineMode in engineModes)
+        {
+            foreach (var pageSegMode in pageSegModes)
+            {
+                try
+                {
+                    using var eng = new TesseractEngine(@"./tessdata", "eng", (EngineMode)Enum.Parse(typeof(EngineMode), engineMode, true));
+                    eng.DefaultPageSegMode = (PageSegMode)Enum.Parse(typeof(PageSegMode), pageSegMode, true);
+
+                    using var memoryStream = new MemoryStream();
+                    file.CopyTo(memoryStream);
+                    var fileBytes = memoryStream.ToArray();
+
+                    using var img = Pix.LoadFromMemory(fileBytes);
+                    using var page = eng.Process(img);
+
+                    var text = page.GetText();
+                    var meanConfidence = page.GetMeanConfidence();
+
+                    result.Add(new OcrResult
+                    {
+                        Text = text,
+                        EngineMode = engineMode,
+                        PageSegMode = pageSegMode,
+                        MeanConfidence = meanConfidence
+                    });
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("EX: " + ex.ToString());
+                }
+            }
+        }
+
+        return result
+            .Where(x => x.MeanConfidence > 0)
+            .OrderByDescending(x => x.MeanConfidence)
+            .ToList();
+    }
+
     [HttpPost]
     public List<OcrResult> Post(IFormFile file)
     {
@@ -42,7 +91,10 @@ public class TesseracController : ControllerBase
                         MeanConfidence = meanConfidence
                     });
                 }
-                catch { }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("EX: " + ex.ToString());
+                }
             }
         }
 
